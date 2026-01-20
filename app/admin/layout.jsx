53c502e8d26ef2d3ccffa96d4bca40880f0a51b5
jonty1231 <jontythakur68@gmail.com>
2026-01-20 09:55:51 +0530

    Ad

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,11 +1,17 @@
+"use client"
+import { usePathname } from 'next/navigation'
 import React from 'react'
+import Sidebar from './Componentsadmin/Slidebar'
 
 const layout = ({ children }) => {
+  const pathname = usePathname()
+
   return (
    <html lang="en">
       <body
  
       >
+<Sidebar />
 
         {children}
    
